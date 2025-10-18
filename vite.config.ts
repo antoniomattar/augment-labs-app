d786eb0f302d7453fae4827f0941426884a94102
Antonio MATTAR <antonio.mattar@grenoble-inp.org>
2025-10-19 00:00:16 +0200

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	base: mode === "development" ? "/" : "/augment-labs-glow/",
+	base: "/augment-labs/",
 	server: {
 		host: true,
 		port: 8080,
