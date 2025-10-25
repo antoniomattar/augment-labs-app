a97bc40598fa07d11622df7cdba21194d98312f5
Antonio MATTAR <antonio.mattar@grenoble-inp.org>
2025-10-25 21:54:58 +0200

    fix 🐛: adjusting base url for our new domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	base: "/augment-labs-app/",
+	base: "/",
 	server: {
 		host: true,
 		port: 8080,
